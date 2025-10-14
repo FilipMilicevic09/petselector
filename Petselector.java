76352c76bf2e9df15f06187d7dfb03fa01f9d5fe
FilipMilicevic09 <filip.milicevic009@gmail.com>
2025-10-14 18:29:06 +0000

    changed code

--- Petselector.java
+++ Petselector.java
@@ -1,1 +1,29 @@
+import java.util.*;
 
+public class Petselector{
+    public static void main (String []args){
+        //ask the user for input (name, season and color)
+        Scanner input = new Scanner(System.in);
+        System.out.println("what is your favorite season?");
+        String season = input.nextLine().toLowerCase().trim();
+        
+        if(!(season.equals("fall")|| season.equals("spring")|| season.equals("winter")|| season.equals("summer"))){
+            System.out.println("not a real season, try again");
+            return;
+        }
+
+        
+        System.out.println("what is your favorite color?");
+        String color = input.nextLine().toLowerCase().trim();
+        
+         if(!(color.equals("red")|| colorsumm.equals("green")|| color.equals("blue"))){
+            System.out.println("not applicable try red, blue, or green");
+            return;
+        }
+        
+        //make sure the input is valid (Idiot proof!)
+        //make a decision
+        //tell the user what they get!
+
+}
+}
