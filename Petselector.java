9d7b4f2af3434232c351a2d07bdbec9efef33762
FilipMilicevic09 <filip.milicevic009@gmail.com>
2025-10-17 01:43:38 +0000

    new

--- Petselector.java
+++ Petselector.java
@@ -1,37 +1,56 @@
 import java.util.*;
 
 public class Petselector{
+    
+    
     public static void main (String []args){
         //ask the user for input (name, season and color)
         Scanner input = new Scanner(System.in);
-        System.out.println("what is your favorite season?");
+        System.out.println("What is your favorite season?");
         String season = input.nextLine().toLowerCase().trim();
         
         if(!(season.equals("fall")|| season.equals("spring")|| season.equals("winter")|| season.equals("summer"))){
-            System.out.println("not a real season, try again");
+            System.out.println("not a real season, try again (run again)");
             return;
         }
 
         
-        System.out.println("what is your favorite color?");
+        System.out.println("What is your favorite color?");
         String color = input.nextLine().toLowerCase().trim();
         
          if(!(color.equals("red")|| color.equals("green")|| color.equals("blue"))){
-            System.out.println("not applicable try red, blue, or green");
+            System.out.println("not applicable try red, blue, or green (run again)");
             return;
         }
-
         
-        System.out.println("what is your Name?");
+        System.out.println("What is your Name?");
         String name = input.nextLine().toLowerCase().trim();
         
-        //make sure the input is valid (Idiot proof!)
-        //make a decision
-        //tell the user what they get!
+    
         if(color.equals("blue") && season.equals ("fall"))
         {
             System.out.println("\nYour perfect pet is: Aligator \n");
             return;
         }
+        else if (color.equals("blue") && season.equals ("spring"))
+        {
+            System.out.println("\nYour perfect pet is: Ostrich \n");
+            return;
+        }
+        else if ( color.equals ("blue") && season.equals("winter") && "aeiouAEIOU".indexOf(name) != 0)
+        {
+            System.out.println("\nYour perfect pet is: Axolotl \n");
+            return;
+        }
+        else
+        {
+          System.out.println("\nYour perfect pet is: doesn't exist \n");
+            return;  
+        }
+
+        
+
+
+
 }
 }
